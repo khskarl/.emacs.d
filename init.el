120fca1add0c8b7725391e6375baa05115a0c816
khskarl <karllhenning64@gmail.com>
2017-04-22 00:33:39 -0300

    Added aggressive mode; Added ido.last to .gitignore

--- init.el
+++ init.el
@@ -2,16 +2,16 @@
 ;; -------------------------------
 
 (require 'package) 
-
+(package-initialize) 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-(package-initialize) 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 (defvar my-packages
-  '(rainbow-mode
+  '(aggressive-indent
+    rainbow-mode
     rainbow-delimiters
     yasnippet
     auto-complete
@@ -21,7 +21,10 @@
     rust-mode
     flycheck-rust
     cargo
-    racer))
+    racer 
+    cider
+    ac-cider
+    lispy))
 
 (mapc #'(lambda (package)
 	  (unless (package-installed-p package)
@@ -38,7 +41,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (racer cargo flycheck-rust rust-mode ## anaconda-mode rainbow-mode rainbow-delimiters auto-complete flycheck rtags company))))
+    (lispy aggressive-indent racer cargo flycheck-rust rust-mode ## anaconda-mode rainbow-mode rainbow-delimiters auto-complete flycheck rtags company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -58,6 +61,8 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'hc-zenburn t)
+
+(global-aggressive-indent-mode 1)
 
 ;; SETUP PYTHON
 ;; ------------------------------
@@ -122,15 +127,33 @@
 
 
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-(add-hook 'rust-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+(add-hook 'rust-mode-hook 'racer-mode)
+(add-hook 'rust-mode-hook 'flycheck-mode)
+(add-hook 'racer-mode-hook 'eldoc-mode)
+(add-hook 'racer-mode-hook 'company-mode)
+(add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
+(add-hook 'rust-mode-hook (lambda () (local-set-key (kbd "C-c <tab>") 'rust-format-buffer)))
 
 (yas-global-mode 1)
 
 ;;(define-key yas-keymap (kbd "<backtab>") 'yas-prev-field)
 (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-prev-field)
+
+;; Customize Clojure
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(add-hook 'clojure-mode-hook 'subword-mode)
+
+;; nREPL customizations
+(setq nrepl-hide-special-buffers t)       ; Don't show buffers like connection or server
+(setq nrepl-popup-on-error nil)           ; Don't popup new buffer for errors (show in nrepl buffer)
+(setq nrepl-popup-stacktraces-in-repl t)                          ; Display stacktrace inline
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode) 
+(add-hook 'nrepl-mode-hook 'paredit-mode)                         
+(add-to-list 'same-window-buffer-names "*nrepl*")               ; Make C-c C-z switch to *nrepl*
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+					;(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+					;(add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+					;(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+(add-hook 'clojure-mode-hook    (lambda () (lispy-mode 1)))
